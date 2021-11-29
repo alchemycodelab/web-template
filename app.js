6df9f979cdec8db6792bcafddc2e0b518ee6fff8
Danny Cairns <dpcairns@gmail.com>
2021-11-28 20:43:42 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// import functions and grab DOM elements
+
+// initialize state
+
+// set event listeners 
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
