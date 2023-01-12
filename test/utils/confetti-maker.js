b3b05e65dc3231b4c9581a414fb106d7e4788049
Danny Cairns <dpcairns@gmail.com>
2023-01-12 12:44:19 -0800

    change naming

--- test/utils/confetti-maker.js
+++ test/utils/confetti-maker.js
@@ -2,14 +2,21 @@
 let confettiHappened = false;
 
 QUnit.done(() => {
-    if (!document.hidden && noProblems()) doConfetti();
+    // when qunit is done, launch confetti if user is on page and all tests pass
+    if (!document.hidden && testsPassed()) doConfetti();
 });
 
 document.addEventListener('visibilitychange', (event) => {
-    if (!document.hidden && !confettiHappened && noProblems()) {
+    /*
+        if due to live-reload the user was off-tab when tests passed,
+        do confetti when they navigate back, assuming confetti hasn't happened 
+        yet and tests are passing
+    */
+    if (!document.hidden && !confettiHappened && testsPassed()) {
         doConfetti();
     } 
 
+    // if user navigates during confetti, confetti goes away
     if (document.hidden) confetti.reset();
 });
 
@@ -24,6 +31,7 @@ function doConfetti() {
     confettiHappened = true;
 }
 
-function noProblems() {
+// check if tests passed
+function testsPassed() {
     return QUnit && QUnit.config && QUnit.config.stats && QUnit.config.stats.bad === 0;
 }
